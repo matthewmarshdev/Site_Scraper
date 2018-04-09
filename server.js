b06a7a770491c68fcaaf7295c9a73b0fb97c5cd4
matthewmarshdev <matthewmarshdev@gmail.com>
2018-04-08 23:42:29 -0600

    final commit

--- server.js
+++ server.js
@@ -148,10 +148,7 @@ app.delete("/articles/:id", function (req, res) {
   // Create a new note and pass the req.body to the entry
   db.Comment.delete(req.body)
     .then(function (dbComment) {
-      // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
-      // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
-      // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
-      return db.Article.findOneAndUpdate(
+      return db.Article.destroy(
         { _id: req.params.id },
         { comment: dbComment._id },
       );
